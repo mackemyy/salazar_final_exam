434eacefa287b5161b8447b00d899f7ae008b832
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-08-03 22:56:00 +0800

    Wrapped the main dart file with BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'app_router.dart';
 import 'app_themes.dart';
+import 'blocs/bloc_exports.dart';
 import 'screens/tabs_screen.dart';
 
 void main() {
-  runApp(
-    MyApp(appRouter: AppRouter()),
-  );
+  BlocOverrides.runZoned(() => runApp(
+      MyApp(appRouter: AppRouter()),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,11 +18,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BloC Tasks App',
-      theme: AppThemes.appThemeData[AppTheme.lightMode],
-      home: const TabsScreen(),
-      onGenerateRoute: appRouter.onGenerateRoute,
+    return BlocProvider(
+      create: (context) => TasksBloc(),
+      child: MaterialApp(
+        title: 'BloC Tasks App',
+        theme: AppThemes.appThemeData[AppTheme.lightMode],
+        home: const TabsScreen(),
+        onGenerateRoute: appRouter.onGenerateRoute,
+      ),
     );
   }
 }
