dde9e5414caecbec3e535047696302f4106c2741
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-08-04 01:57:59 +0800

    Implemented HydratedBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app_router.dart';
 import 'app_themes.dart';
 import 'blocs/bloc_exports.dart';
 import 'screens/tabs_screen.dart';
 
-void main() {
-  BlocOverrides.runZoned(() => runApp(
-      MyApp(appRouter: AppRouter()),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory(),
+    );
+
+  HydratedBlocOverrides.runZoned(
+    () => runApp(MyApp(appRouter: AppRouter())),
+        storage: storage,
+      );
 }
 
 class MyApp extends StatelessWidget {
